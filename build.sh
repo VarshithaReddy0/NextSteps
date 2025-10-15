2f5450090f66f58c2f4fcc0e1176bccfbfc81d9b
Velma SaiVarshitha <velma_saivarshitha@epam.com>
2025-10-15 16:05:46 +0530

    build

--- build.sh
+++ build.sh
@@ -3,22 +3,25 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Force reset migration state
 python -c "
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from your_app import app, db
-with app.app_context():
-    db.engine.execute('DELETE FROM alembic_version;')
-    print('Cleared migration history')
+import os
+import psycopg2
+from urllib.parse import urlparse
+
+url = urlparse(os.environ['DATABASE_URL'])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+cur = conn.cursor()
+cur.execute('DROP TABLE IF EXISTS alembic_version;')
+conn.commit()
+conn.close()
+print('Reset migration tracking')
 "
 
-# Get current head and stamp
-HEAD=$(flask db heads | head -n1 | awk '{print $1}')
-flask db stamp $HEAD
-
-# Run migrations
+flask db stamp head
 flask db upgrade
-
-# Create admin user
-python init_admin.py
