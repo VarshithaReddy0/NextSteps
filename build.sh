eaab0dda89f1517211edb8d3d4498203bc9e050f
Velma SaiVarshitha <velma_saivarshitha@epam.com>
2025-10-15 15:52:13 +0530

    added python scipt in build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
-# Run migrations with fallback
-flask db upgrade || (flask db stamp head && flask db upgrade)
+# Force reset migration state
+python -c "
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from your_app import app, db
+with app.app_context():
+    db.engine.execute('DELETE FROM alembic_version;')
+    print('Cleared migration history')
+"
+
+# Get current head and stamp
+HEAD=$(flask db heads | head -n1 | awk '{print $1}')
+flask db stamp $HEAD
+
+# Run migrations
+flask db upgrade
 
 # Create admin user
 python init_admin.py
